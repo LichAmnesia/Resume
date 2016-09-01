b408b76584f7de96176c53e6b9b3cdf43b2ccfda
Lich_Amnesia <alwaysxiaop@gmail.com>
2016-09-01 13:58:04 -0600

    add delete compile files

---
+++ run.bat
@@ -0,0 +1,23 @@
+@REM @Author: Lich_Amnesia
+@REM @Date:   2016-08-28 20:53:19
+@REM @Last Modified by:   Alwa
+@REM Modified time: 2016-08-31 14:34:00
+rem xelatex b.tex
+rem bibtex b.aux
+rem xelatex b.tex
+rem xelatex b.tex
+
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+del *.toc /s
+del *.out /s
+del *.lof /s
+del *.lol /s
+del *.lot /s
+del *.fdb_latexmk /s
+del *.synctex.gz /s
