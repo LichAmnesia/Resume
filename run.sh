e97be841157f737ee506c73d6db62551afc351a3
Lich_Amnesia <alwaysxiaop@gmail.com>
2017-09-03 20:38:35 -0600

    update resume with intern

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 # @Author: lich
 # @Date:   2017-07-13 21:54:42
-# @Last Modified by:   lich
-# @Last Modified time: 2017-07-13 22:17:28
-# 
+# @Last Modified by:   Lich_Amnesia
+# @Last Modified time: 2017-08-19 19:42:40
+
+
+rm *.aux
+rm *.bak
+rm *.log
+rm *.bbl
+rm *.dvi
+rm *.blg
+rm *.thm
+rm *.toc
+rm *.out
+rm *.lof
+rm *.lol
+rm *.lot
+rm *.fdb_latexmk
+rm *.synctex.gz
+rm *.fls
+rm *.xdv
